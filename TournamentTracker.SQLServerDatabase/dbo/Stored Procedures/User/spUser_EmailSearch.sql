e048adbb4fe2c0185f112a21a46d38b181be9654
sadegh <sadegh15khedry@gmail.com>
2023-08-19 10:13:19 +0330

    added user register and login to ther api

---
+++ TournamentTracker.SQLServerDatabase/dbo/Stored Procedures/User/spUser_EmailSearch.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spUser_EmailSearch]
+	@Email nvarchar(200)	
+AS
+BEGIN
+	SELECT Id, FirstName, LastName, Email, Role From dbo.[User]
+	WHERE Email = @Email
+END
---
+++ TournamentTracker.SQLServerDatabase/dbo/Stored Procedures/User/spUser_GetByEmail.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spUser_GetByEmail]
+	@Email nvarchar(200)	
+AS
+BEGIN
+	SELECT Id, FirstName, LastName, Email, Role, Password From dbo.[User]
+	WHERE Email = @Email
+END
